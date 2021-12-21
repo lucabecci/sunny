24019e142809fc2464384b35208e126c68d861e9
lucabecci <beccibrian@gmail.com>
2021-12-21 20:54:19 -0300

    feat(): change perror to std library and comments added in all steps

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include<bits/stdc++.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -9,47 +10,53 @@
 
 int main(int argc, char *argv[], char **envp)
 {
+  //Variables necesarias para la creacion del socket y su funcionamiento
   int server_fd, new_socket, valread;
   struct sockaddr_in address;
   int opt = 1;
   int addrlen = sizeof(address);
   char buffer[1024] = {0};
-  char *hello = "Hello from server";
+  char *hello  = "Hello from server";
 
+  //Creando un socket file descriptor
   if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
   {
-    perror("socket failed...");
-    exit(EXIT_FAILURE);
+    std::cout << "socket failed..." << std::endl;
+    exit(EXIT_FAILURE); 
   }
-
+  //Creando una conexion forzada al puerto 8080
   if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
   {
-    perror("setsockopt");
+    std::cout << "setsockopt" << std::endl;
     exit(EXIT_FAILURE);
   }
-
+  //Definiendo varibales de conexion del socket
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons( PORT );
-  
+   //Bindeando la conexion forzada al puerto 8080
   if(bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
   {
-    perror("Bind failed");
+    std::cout << "Bind failed" << std::endl;;
     exit(EXIT_FAILURE);
   }
+  //Maximas conexiones al socket
   if(listen(server_fd, 3) < 0)
   {
-    perror("Listen");
+    std::cout << "Listen" << std::endl;
     exit(EXIT_FAILURE);
   }
+  //Aceptacion de conexiones al socket
   if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
   {
-    perror("Accept");
+    std::cout << "Accept" << std::endl;
     exit(EXIT_FAILURE);
   }
+  //Leyendo el buffer entrante al socket
   valread = read(new_socket, buffer, 1024);
   printf("%s\n",buffer );
+  //Enviando un mensaje al cliente
   send(new_socket , hello , strlen(hello) , 0 );
-  printf("Hello message sent\n");
+  std::cout << "Hello message sent" << std::endl;
   return 0;
 }
