71ffe1c979893616f9ceb4d86c2e09b16bf989d8
lucabecci <beccibrian@gmail.com>
2022-01-04 01:21:55 -0300

    feat(): more colors in colors.h

--- src/utils/colors.h
+++ src/utils/colors.h
@@ -1,5 +1,13 @@
 #include <cstring>
-
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
 #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
@@ -26,6 +34,18 @@ std::string colors::change(int type)
             return BOLDGREEN;
         case 4: 
             return BOLDBLUE;
+        case 5:
+            return WHITE;
+        case 6: 
+            return RED;
+        case 7:
+            return YELLOW;
+        case 8:
+            return GREEN;
+        case 9:
+            return BLUE;
+        case 10:
+            return MAGENTA;
         default:
             return BOLDWHITE;
     }
