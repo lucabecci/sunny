e36e83fcc1771148ebb101d0e89585727efa51d1
lucabecci <beccibrian@gmail.com>
2022-01-04 01:40:35 -0300

    fix(): white color value in colors.h

--- src/utils/colors.h
+++ src/utils/colors.h
@@ -1,5 +1,5 @@
 #include <cstring>
-#define RESET   "\033[0m"
+#define WHITE   "\033[0m"       /* White */
 #define BLACK   "\033[30m"      /* Black */
 #define RED     "\033[31m"      /* Red */
 #define GREEN   "\033[32m"      /* Green */
@@ -7,7 +7,6 @@
 #define BLUE    "\033[34m"      /* Blue */
 #define MAGENTA "\033[35m"      /* Magenta */
 #define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
 #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
