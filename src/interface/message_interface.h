a1d54bae874a18d0927538d1e95b084d91de543f
lucabecci <beccibrian@gmail.com>
2022-01-03 23:23:42 -0300

    feat(): changes in message_interface::stdout

--- src/interface/message_interface.h
+++ src/interface/message_interface.h
@@ -1,35 +1,28 @@
 #include<cstring>
 #include "../utils/colors.h"
-
-#define BOLDWHITE   0      /* Bold White */
-#define BOLDRED     1      /* Bold Red */
-#define BOLDGREEN   2     /* Bold Green */
-#define BOLDYELLOW  3     /* Bold Yellow */
-#define BOLDBLUE    4      /* Bold Blue */
-
 class message_interface
 {
     private:
         colors colors_process;
     public:
         void stdout(std::string message, std::string type);
-}
+};
 
 
 void message_interface::stdout(std::string message, std::string type)
 {
     std::string color = colors_process.change(0);
-    if(type == "NORMAL") scolors_process.change(0);
-    else if(type == "ERROR") colors_process.change(1);
-    else if(type == "SUCCESS") colors_process.change(2);
-    else if(type == "WARNING") colors_process.change(3);
-    else if(type == "INFO") colors_process.change(4);
+    if(type == "NORMAL") std::cout << colors_process.change(0) << message;
+    else if(type == "ERROR") std::cout << colors_process.change(1) << message;
+    else if(type == "WARNING") std::cout << colors_process.change(2) << message;
+    else if(type == "SUCCESS") std::cout << colors_process.change(3) << message;
+    else if(type == "INFO") std::cout << colors_process.change(4) << message;
     else 
     {
         std::cout << colors_process.change(1) << "Color not found"<< std::endl;
-        colors_process.change(0);    
+        std::cout << colors_process.change(0) << message;
     }
-    std::cout << message << std::endl;
+    std::cout << colors_process.change(0) << std::endl;
     return;
 }
 
