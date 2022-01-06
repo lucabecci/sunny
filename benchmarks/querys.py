ab55582d45b51e469d3980a68938e758f2770f51
lucabecci <beccibrian@gmail.com>
2022-01-06 01:06:52 -0300

    refactor(): one find in 5000 docs

--- benchmarks/querys.py
+++ benchmarks/querys.py
@@ -2,29 +2,34 @@ import socket
 import sys
 import uuid
 from random import seed
-from random import random
+import random
 import time
 
-import asyncio
 
 seed(1)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def bench():
     arr = []
-    values = []
     print("Bench started")
     connection()
-    for x in range(40):
+    for x in range(500000):
         v = str(uuid.uuid4())
         arr.append(v)
-        t = str(random())
-        values.append(t)
+        t = str(random.random())
         query_put = 'put ' + v + " " + t
         print("Inserted " + query_put)
         sock.send(query_put.encode("utf-8"))
-        time.sleep(0.05)
-    return
+        time.sleep(0.02)
+    
+    index = random.choice(arr)
+    query = "get " + index
+    start_time = time.time()
+    sock.send(query.encode())
+    data = sock.recv(1024)
+    print("VALUE: " + data.decode())
+    print("TIME TO FIND THE KEY IN THE DB")
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 def connection():
     server_address = ('localhost', 8080)
