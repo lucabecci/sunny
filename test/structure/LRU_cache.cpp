a824d11cfe1a9af7ca031a2cffe205c9bbc34887
lucabecci <beccibrian@gmail.com>
2022-05-06 18:57:02 -0300

    test(LRU): unit tests in LRU algorithm

---
+++ test/structure/LRU_cache.cpp
@@ -0,0 +1,24 @@
+#define CATCH_CONFIG_MAIN 
+
+#include "catch.hpp"
+#include "../../src/structure/LRU_cache.h"
+
+LRU_cache< std::string, std::string > lru;
+
+TEST_CASE("put and get value in the lru - first", "[LRU]")
+{
+    lru.put("Key_V", "string_to_find");
+    REQUIRE(lru.get("Key_V") == "string_to_find");
+}
+
+TEST_CASE("put and get value in the lru - second", "[LRU]")
+{
+    lru.put("ERR_KV", "A");
+    REQUIRE(lru.get("ERR_KV") != "2");
+}
+
+TEST_CASE("put and get value in the lru - third", "[LRU]")
+{
+    REQUIRE(lru.get("NOT_KV") == "NULL_V_SUNNY");
+}
+
