a45a98070058dee5c064acd5c7bce6713537fe50
lucabecci <beccibrian@gmail.com>
2022-05-08 17:31:06 -0300

    refactor(managment): macro changed

--- includes/memory/managment.hpp
+++ includes/memory/managment.hpp
@@ -1,14 +1,14 @@
 #ifndef MANAGMENT
 #define MANAGMENT 
 
-#define MAX 1024
+#define MAX_MNG 1024
 
 extern int sockfd;
 class managment
 {
   private:
     std::string first(std::string str);
-    char buff[MAX];
+    char buff[MAX_MNG];
   public:
     std::string pst(std::string f, bool capture);
     std::string validate(std::string command);
